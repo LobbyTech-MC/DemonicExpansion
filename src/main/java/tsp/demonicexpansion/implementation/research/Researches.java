3f4bc0544bbb53b2ec3b0ffb6b078fbd610aa7a3
Silent <perovskiboki@mail.com>
2022-06-12 13:00:49 +0200

    Researches

--- src/main/java/tsp/demonicexpansion/implementation/research/Researches.java
+++ src/main/java/tsp/demonicexpansion/implementation/research/Researches.java
@@ -17,7 +17,9 @@ public class Researches {
                 items.DEMONIC_ESSENCE.getItem(),
                 items.DEMONIC_HELMET.getItem(), items.DEMONIC_CHESTPLATE.getItem(), items.DEMONIC_LEGGINGS.getItem(), items.DEMONIC_BOOTS.getItem(),
                 items.PENTECOSTAL_COIN.getItem(),
-                items.THERMAL_GENERATOR.getItem()
+                items.THERMAL_GENERATOR.getItem(),
+                items.DEVILS_RING.getItem(),
+                items.NAPALM.getItem()
         );
         HELL.register();
     }
